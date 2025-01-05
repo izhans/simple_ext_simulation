7c2582a4ceaa59671c6f5977b29c94bc35885318
izhans <izhansastre@gmail.com>
2025-01-05 22:11:42 +0100

    refactor imprimir

--- src/imprimir.c
+++ src/imprimir.c
@@ -12,33 +12,28 @@ EXT_DATOS GetBloque(EXT_DATOS *memdatos, int i_nbloque);
  */
 int	Imprimir(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos, EXT_DATOS *memdatos, char *nombre)
 {
-	int i;
+	int entrada_fichero;
 	int inodo;
 	EXT_SIMPLE_INODE fichero;
 	EXT_DATOS bloque;
 
-	i = 1; // empieza en 1 por que la entrada 0 es la del directorio (.)
-	while (i < MAX_FICHEROS) // recorro las 20 entradas (posibles ficheros) del directorio
+	entrada_fichero = BuscaFich(directorio, inodos, nombre);
+	if (!entrada_fichero)
 	{
-		// si la entrada del directorio tiene un inodo significa que hay un fichero
-		if (strcmp(nombre, directorio[i].dir_nfich) == 0)
-		{
-			inodo = directorio[i].dir_inodo;
-			fichero = inodos->blq_inodos[inodo];
-			
-			// recorro la lista de bloques que contiene el inodo y voy imprimiendo su contenido
-			for (size_t j = 0; fichero.i_nbloque[j] != NULL_BLOQUE; j++)
-			{
-				bloque = GetBloque(memdatos, fichero.i_nbloque[j]);
-				printf("%s", bloque.dato);
-			}
-			printf("\n");
-			return (0);
-		}
-		i++;
+		printf("ERROR: No se ha encontrado el fichero %s\n", nombre);
+		return (1);
 	}
-	printf("ERROR: No se ha encontrado el fichero %s\n", nombre);
-	return (1);
+	inodo = directorio[entrada_fichero].dir_inodo;
+	fichero = inodos->blq_inodos[inodo];
+	
+	// recorro la lista de bloques que contiene el inodo y voy imprimiendo su contenido
+	for (size_t j = 0; fichero.i_nbloque[j] != NULL_BLOQUE; j++)
+	{
+		bloque = GetBloque(memdatos, fichero.i_nbloque[j]);
+		printf("%s", bloque.dato);
+	}
+	printf("\n");
+	return (0);
 }
 
 /**
